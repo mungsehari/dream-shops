ed3b193724a38429aba7da16c52c67dd089cdf2b
mungsehari <mungse.hari@gmail.com>
2024-09-05 19:41:14 +0530

    update a Order

--- src/main/java/com/hari/model/Order.java
+++ src/main/java/com/hari/model/Order.java
@@ -7,16 +7,7 @@ import java.util.Set;
 
 import com.hari.enums.OrderStatus;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,16 +18,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private LocalDate orderDate;
     private BigDecimal totalAmount;
-
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
-
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<OrderItem> orderItems = new HashSet<>();
 
