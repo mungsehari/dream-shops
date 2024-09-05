8ad4be2a07706d9ed979c583e7bc9bd2c3120053
mungsehari <mungse.hari@gmail.com>
2024-09-05 19:43:12 +0530

    Update UserService

--- src/main/java/com/hari/service/user/UserService.java
+++ src/main/java/com/hari/service/user/UserService.java
@@ -1,5 +1,6 @@
 package com.hari.service.user;
 
+import com.hari.dto.UserDto;
 import com.hari.model.User;
 import com.hari.request.CreateUserRequest;
 import com.hari.request.UserUpdateRequest;
@@ -8,10 +9,11 @@ public interface UserService {
 
     User getUserById(Long UserId);
 
-    User CreateUser(CreateUserRequest request);
+    User createUser(CreateUserRequest request);
 
     User updateUser(UserUpdateRequest request, Long userId);
 
     void deleteUser(Long userId);
 
+    UserDto convertUserToDto(User user);
 }
