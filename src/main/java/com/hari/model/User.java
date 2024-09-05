6e9b312becf640e7b61a6d6f113e29a01e2228ca
mungsehari <mungse.hari@gmail.com>
2024-09-05 19:42:21 +0530

    update a User model

--- src/main/java/com/hari/model/User.java
+++ src/main/java/com/hari/model/User.java
@@ -2,29 +2,26 @@ package com.hari.model;
 
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
+    @NaturalId
     private String email;
     private String password;
 
