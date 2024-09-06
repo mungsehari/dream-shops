4a4761aea9d6864c24c730ae50eed951a5685706
mungsehari <mungse.hari@gmail.com>
2024-09-06 20:26:11 +0530

    create ShopUserDetailsService

---
+++ src/main/java/com/hari/security/user/ShopUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.hari.security.user;
+
+import com.hari.model.User;
+import com.hari.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user= Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(()->new UsernameNotFoundException("User not found"));
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
