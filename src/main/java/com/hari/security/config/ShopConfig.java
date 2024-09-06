c45d6f7bad8ba37aad94a81e5828a21a08cc074a
mungsehari <mungse.hari@gmail.com>
2024-09-06 20:25:40 +0530

    update ShopConfig

---
+++ src/main/java/com/hari/security/config/ShopConfig.java
@@ -0,0 +1,81 @@
+package com.hari.security.config;
+
+import com.hari.security.jwt.AuthTokenFilter;
+import com.hari.security.jwt.JwtAuthEntryPoint;
+import com.hari.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class ShopConfig {
+    private final ShopUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint authEntryPoint;
+
+    private static final List<String> SECURED_URL =
+            List.of("/api/carts/**","/api/cartItems/**");
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+
+        return authConfig.getAuthenticationManager();
+
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        var authProvider=new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception->exception.authenticationEntryPoint(authEntryPoint))
+                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth->auth.requestMatchers(SECURED_URL.toArray(String[]::new))
+                                        .authenticated().anyRequest().permitAll());
+
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+
+}
